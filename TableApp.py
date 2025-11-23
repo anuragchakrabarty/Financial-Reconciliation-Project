659846f8fffb4f7e509e4d6f277145ccc59089d7
anuragchakrabarty <anuragchakrabartyofficial@gmail.com>
2025-11-23 11:19:44 +0530

    Created Table Creation File. Its rough but  working, and able to create table in my local database.

--- TableApp.py
+++ TableApp.py
@@ -7,12 +7,4 @@ st.title('Table Creation Page')
 uploadedFile=st.file_uploader("Upload File")
 
 if uploadedFile:
-    uploadedFileDict = tc.TableInputs(uploadedFile,pd)
-    # {'fileName': 'test.csv', 'fileType': 'text/csv', 'columnsAll': defaultdict(None, {'STID': 'INT', 'FIRSTCOLUMN': 'VARCHAR', 'SECONDCOLUMN': 'VARCHAR', 'LASTCOLUMN': 'INT'})}
-    
-    datatype=('VARCHAR','INT','STRING','FLOAT')
-
-    if 'fileName' not in st.session_state:
-        st.session_state['fileName']=uploadedFileDict['fileName']
-    
-    fileNameInput=st.text_input("File Name : ",value=st.session_state['fileName'],key='fileName')
+    tc.TableOperations(uploadedFile,pd,st,tc)      
--- TableCreation.py
+++ TableCreation.py
@@ -1,17 +1,16 @@
 import mysql.connector
 from mysql.connector import Error
 
-def create_connection(self):
+def create_connection():
     try:
-        self.connection = mysql.connector.connect(
+        connection = mysql.connector.connect(
             host="127.0.0.1",        # e.g., "127.0.0.1"
             user="root",
             password="root",
             database="swift_recon"
         )
-        if self.connection.is_connected():
-            print("Connected to Swift Recon DataBase")
-            return self.connection
+        if connection.is_connected():
+            return connection
     except Error as e:
         print("Error:", e)
         return None
@@ -25,6 +24,16 @@ def create_connection(self):
 
 # a=check_connection()
 # print(a)
+
+def querydType(option):
+    if option.lower()=='varchar':
+        return 'VARCHAR(200) DEFAULT NULL'
+    elif option.lower()=='int':
+        return 'INT DEFAULT NULL'
+    elif option.lower()=='float':
+        return 'FLOAT DEFAULT NULL'
+    elif option.lower()=='datetime':
+        return 'DATETIME DEFAULT NULL'
 
 def TableInputs(uploadedFile,pd):
     from collections import defaultdict
@@ -69,5 +78,64 @@ def TableInputs(uploadedFile,pd):
     
     return {'fileName':FileName,'fileType':FileType,'columnsAll':columns}
 
-def TableCreation():
-    return
+def TableCreation(query):
+    connection=create_connection()
+    creationStatus=None
+    if connection:
+        cursor=connection.cursor(dictionary=True)
+        try:
+            cursor.execute(query, params=None)
+            connection.commit()
+
+            creationStatus='Table Created Successfully'
+        
+        except Error as e:
+            creationStatus=f"Error Executing Query : {e}"
+
+        finally:
+            cursor.close()
+            connection.close()
+
+        return creationStatus
+    
+def TableOperations(uploadedFile,pd,st,tc):
+    uploadedFileDict = tc.TableInputs(uploadedFile,pd)
+    
+    dataTypeOptions=('VARCHAR','INT','FLOAT','DATETIME')
+    fileTypeOptions=("CSV","EXCEL")
+
+    if 'fileName' not in st.session_state:
+        st.session_state['fileName']=uploadedFileDict['fileName']
+    
+    if 'fileType' not in st.session_state:
+        st.session_state['fileType']=uploadedFileDict['fileType']
+    
+    fileNameInput=st.text_input(label="File Name : ",value=st.session_state['fileName'],key='fileName')
+    fileTypeInput=st.selectbox(label="File Type : ",options=fileTypeOptions,key='fileType')
+
+    tableQueryElements=[]
+    with st.form(key='Column DataType Selection', clear_on_submit=False, enter_to_submit=False):
+
+        startQuery=f"CREATE TABLE IF NOT EXISTS {fileNameInput} ( ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, "
+        
+        for key,value in uploadedFileDict['columnsAll'].items():
+            dataType=value
+            with st.container():
+                st.write(f'{key}')
+                dTypeOption=st.selectbox(label='Data Type : ',options=dataTypeOptions,key=f'{dataType} {key}')
+                
+                dType=tc.querydType(dTypeOption)
+                query=f"{key} {dType}"
+
+                tableQueryElements.append(query)
+        
+        midQuery=", ".join(tableQueryElements)
+        lastQuery=")"            # Need to add Application Table Columns Status, Recon Iteration Id, Pipeline Execution Id, later on which will also require to link as Foreign Keys
+        fullQuery=startQuery+midQuery+lastQuery
+
+        submitted=st.form_submit_button("Create Table")
+        if submitted:
+            st.write(fullQuery)
+            print(fullQuery)
+            creationStatus=tc.TableCreation(fullQuery)
+            st.toast(creationStatus,duration='infinite')
